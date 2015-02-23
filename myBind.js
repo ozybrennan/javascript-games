e3e8dc7de476b6de86a902e8711df093ad6d10b1
Ozy Brennan <ozybrennan@users.noreply.github.com>
2015-02-23 15:05:22 -0800

    Update myBind.js

--- myBind.js
+++ myBind.js
@@ -4,17 +4,3 @@ Function.prototype.myBind = function (context) {
     fn.apply(context);
   };
 }
-
-var Snowman = function (name) {
-  this.name = name;
-};
-
-var melt = function () {
-  console.log(this.name + " is melting.")
-};
-
-olaf = new Snowman("olaf")
-
-myBoundFunction = melt.myBind(olaf)
-
-myBoundFunction();
